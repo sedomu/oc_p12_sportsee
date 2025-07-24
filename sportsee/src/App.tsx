adc4de6aa7db45618dac6b16390d245e77bc615c
sedomu <seguin@lilo.org>
2025-07-24 16:31:48 +0200

    Add StatsGraph component for displaying nutrition data with icons in App layout.

--- sportsee/src/App.tsx
+++ sportsee/src/App.tsx
@@ -2,59 +2,79 @@ import "./styles/main.css"
 import useFetch from './hooks/useFetch'
 import DailyActivityGraph from "./components/DailyActivityGraph.tsx";
 import {type JSX, type Ref, type RefObject, useRef} from "react";
+import StatsGraph from "./components/StatsGraph.tsx";
 
 function App() {
 
 
     const data = useFetch({userId: 12, mocked: true})
 
-  
-  return (
-    <>
-        <header className="header">
-            <nav className="header__menu">
-                <ul>
-                    <li><img className="header__logo" src="./assets/logo.svg" alt="Sportsee logo"/></li>
-                    <li>Accueil</li>
-                    <li>Profil</li>
-                    <li>Réglages</li>
-                    <li>Communauté</li>
-                </ul>
-            </nav>
-        </header>
+    console.log(data)
 
-        <main>
-            <div className="leftbar">
-                <div className="leftbar__badges">
+    return (
+        <>
+            <header className="header">
+                <nav className="header__menu">
                     <ul>
-                        <li><img className="leftbar__badges--icon" src="./assets/badge_relaxation.svg"/></li>
-                        <li><img className="leftbar__badges--icon" src="./assets/badge_swim.svg"/></li>
-                        <li><img className="leftbar__badges--icon" src="./assets/badge_bike.svg"/></li>
-                        <li><img className="leftbar__badges--icon" src="./assets/badge_fitness.svg"/></li>
+                        <li><img className="header__logo"
+                                 src="./assets/logo.svg" alt="Sportsee logo"/>
+                        </li>
+                        <li>Accueil</li>
+                        <li>Profil</li>
+                        <li>Réglages</li>
+                        <li>Communauté</li>
                     </ul>
-                </div>
-                <div className="leftbar__copyright">Copyright, SportSee 2020</div>
-            </div>
-            <div className="content">
-                <div className="content__greetings">
-                    <p className="hello">Bonjour <span className="firstname">{data && data.firstName}</span></p>
-                    <p className="encouragement">Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
-                </div>
-                <div className="content__graphs">
-                    <div className="content__graphs--dailyActivity">
-                        {data && <DailyActivityGraph data={data.lastSessions}/>}
+                </nav>
+            </header>
+
+            <main>
+                <div className="leftbar">
+                    <div className="leftbar__badges">
+                        <ul>
+                            <li><img className="leftbar__badges--icon"
+                                     src="./assets/badge_relaxation.svg"/></li>
+                            <li><img className="leftbar__badges--icon"
+                                     src="./assets/badge_swim.svg"/></li>
+                            <li><img className="leftbar__badges--icon"
+                                     src="./assets/badge_bike.svg"/></li>
+                            <li><img className="leftbar__badges--icon"
+                                     src="./assets/badge_fitness.svg"/></li>
+                        </ul>
                     </div>
-                    <div className="content__graphs--stats"></div>
-                    <div className="content__graphs--averageSessions">
-                        {data && <DailyActivityGraph data={data.lastSessions}/>}
+                    <div className="leftbar__copyright">Copyright, SportSee
+                        2020
                     </div>
-                    <div className="content__graphs--categories"></div>
-                    <div className="content__graphs--score"></div>
                 </div>
-            </div>
-        </main>
-    </>
-  )
+                <div className="content">
+                    <div className="content__greetings">
+                        <p className="hello">Bonjour <span
+                            className="firstname">{data && data.firstName}</span>
+                        </p>
+                        <p className="encouragement">Félicitation ! Vous avez
+                            explosé vos objectifs hier 👏</p>
+                    </div>
+                    <div className="content__graphs">
+                        <div className="content__graphs--dailyActivity">
+                            {data &&
+                              <DailyActivityGraph data={data.lastSessions}/>}
+                        </div>
+                        <div className="content__graphs--stats">
+                            <StatsGraph iconSrc={"./assets/calories-icon.svg"} figure={data && data.calories} unit={"kCal"} intakeText={"Calories"}/>
+                            <StatsGraph iconSrc={"./assets/protein-icon.svg"} figure={data && data.protein} unit={"g"} intakeText={"Protéines"}/>
+                            <StatsGraph iconSrc={"./assets/carbs-icon.svg"} figure={data && data.carbohydrate} unit={"g"} intakeText={"Glucides"}/>
+                            <StatsGraph iconSrc={"./assets/fat-icon.svg"} figure={data && data.lipid} unit={"g"} intakeText={"Lipides"}/>
+                        </div>
+                        <div className="content__graphs--averageSessions">
+                            {data &&
+                              <DailyActivityGraph data={data.lastSessions}/>}
+                        </div>
+                        <div className="content__graphs--categories"></div>
+                        <div className="content__graphs--score"></div>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 }
 
 export default App
---
+++ sportsee/src/components/StatsGraph.tsx
@@ -0,0 +1,13 @@
+export default function StatsGraph({iconSrc, figure, unit, intakeText}) {
+    const figureText = new Intl.NumberFormat('en-EN').format(figure) + unit
+
+    return <>
+        <div className="content__graphs--stats-container">
+            <img className="content__graphs--stats-icon" src={iconSrc}></img>
+            <div className="content__graphs--stats-data">
+                <div className="content__graphs--stats-data-figure">{figureText}</div>
+                <div className="content__graphs--stats-data-intake">{intakeText}</div>
+            </div>
+        </div>
+    </>
+}
